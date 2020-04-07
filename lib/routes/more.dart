f8930288fdb8793a1f265b7d4ef9b3a74a475aaf
themindstorm <parth.kabra@gmail.com>
2020-04-08 00:21:54 +0800

    removed useless imports

--- lib/routes/more.dart
+++ lib/routes/more.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:nextbussg/components/more/more_buttons_list.dart';
-import 'package:nextbussg/styles/values.dart';
 import 'package:nextbussg/utils/extensions.dart';
 import 'package:nextbussg/components/more/settings/settings_list.dart';
 import 'package:nextbussg/components/core/title_text.dart';
 import 'package:nextbussg/components/core/line.dart';
 import 'package:nextbussg/components/core/page_template.dart';
-import 'package:nextbussg/components/core/space.dart';
 import 'package:nextbussg/utils/strings.dart';
 
 class MorePage extends StatelessWidget {
@@ -14,12 +12,9 @@ class MorePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return PageTemplate(children: [
       TitleText(title: Strings.moreTitle.toUpperCase()).sliverToBoxAdapter(),
-      // Spacing(height: Values.marginBelowTitle).sliver(),
       SettingsList(),
 
-      // Spacing(height: Values.marginBelowTitle).sliver(),
       Line().sliverToBoxAdapter(),
-      // Spacing(height: Values.marginBelowTitle).sliver(),
       
       MoreButtonsList(),
     ]);
