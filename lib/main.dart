85ebd612c63077fe344ba795bae33933d65b3236
themindstorm <parth.kabra@gmail.com>
2020-04-07 20:49:17 +0800

    split main into different files

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
-import 'package:flutter/services.dart';
-import 'package:nextbussg/bounce_scroll.dart';
-import 'package:nextbussg/components/onboarding/introduction_screen.dart';
+
+
 import 'package:nextbussg/providers/favorites.dart';
 import 'package:nextbussg/providers/home_rebuilder.dart';
 import 'package:nextbussg/providers/locationPerms.dart';
 import 'package:nextbussg/providers/search.dart';
-import 'package:nextbussg/quick_actions.dart';
-import 'package:nextbussg/styles/theme.dart';
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:nextbussg/utils/theme_enum.dart';
 import 'package:provider/provider.dart';
-import 'package:nextbussg/tabbed_app.dart';
-import 'package:bot_toast/bot_toast.dart';
-import 'package:quick_actions/quick_actions.dart';
-import 'dart:io' show Platform;
+
 
 void main() async {
   await Hive.initFlutter();
@@ -42,62 +37,8 @@ class MyApp extends StatelessWidget {
             create: (_) => LocationPermissionsProvider()),
         ChangeNotifierProvider<HomeRebuilderProvider>(create: (_) => HomeRebuilderProvider()),
       ],
-      child: MainApp(),
+      child: RouteApp(),
     );
   }
 }
 
-class MainApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      // to change theme and put away onboarding screen
-      valueListenable: Hive.box('settings').listenable(keys: ['theme']),
-      builder: (context, box, widget) {
-        var theme = box.get('theme', defaultValue: ThemeEnum.light);
-
-        // check if this is the first time using the app
-        var settingsBox = Hive.box('settings');
-        bool firstLaunch = settingsBox.get('first_launch', defaultValue: true);
-
-        // set firstLaunch to false so that the onboarding view does not show
-        Widget home;
-        if (firstLaunch)
-          home = new OnboardingView();
-        else
-          home = new TabbedApp();
-
-        // change status bar color accordingly, fix for android
-        if (Platform.isAndroid) {
-          if (theme == ThemeEnum.light) {
-            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarIconBrightness: Brightness.dark,
-            ));
-          } else {
-            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarIconBrightness: Brightness.light,
-            ));
-          }
-        }
-
-        // quick actions
-        final QuickActions quickActions = QuickActions();
-        setup(context, quickActions);
-
-        return BotToastInit(
-          child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: theme == ThemeEnum.dark ? appDarkTheme : appLightTheme,
-            home: ScrollConfiguration(
-              child: home,
-              behavior: BounceScrollBehavior(),
-            ),
-            navigatorObservers: [BotToastNavigatorObserver()],
-          ),
-        );
-      },
-    );
-  }
-}
---
+++ lib/route_app.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:nextbussg/tabbed_app.dart';
+import 'package:bot_toast/bot_toast.dart';
+import 'package:nextbussg/utils/theme_enum.dart';
+import 'package:quick_actions/quick_actions.dart';
+import 'dart:io' show Platform;
+import 'package:nextbussg/quick_actions.dart';
+import 'package:nextbussg/styles/theme.dart';
+import 'package:nextbussg/bounce_scroll.dart';
+import 'package:nextbussg/components/onboarding/introduction_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class RouteApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      // to change theme and put away onboarding screen
+      // valueListenable: Hive.box('settings').listenable(),
+      valueListenable: Hive.box('settings').listenable(),
+      builder: (context, box, widget) {
+        var theme = box.get('theme', defaultValue: ThemeEnum.light);
+
+        // check if this is the first time using the app
+        var settingsBox = Hive.box('settings');
+        bool firstLaunch = settingsBox.get('first_launch', defaultValue: true);
+
+        // set firstLaunch to false so that the onboarding view does not show
+        Widget home;
+        if (firstLaunch)
+          home = new OnboardingView();
+        else {
+          home = new TabbedApp();
+
+          // quick actions
+          final QuickActions quickActions = QuickActions();
+          setup(context, quickActions);
+        }
+
+        // change status bar color accordingly, fix for android
+        if (Platform.isAndroid) {
+          if (theme == ThemeEnum.light) {
+            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+              statusBarIconBrightness: Brightness.dark,
+            ));
+          } else {
+            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+              statusBarIconBrightness: Brightness.light,
+            ));
+          }
+        }
+
+        return BotToastInit(
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: theme == ThemeEnum.dark ? appDarkTheme : appLightTheme,
+            home: ScrollConfiguration(
+              child: home,
+              behavior: BounceScrollBehavior(),
+            ),
+            navigatorObservers: [BotToastNavigatorObserver()],
+          ),
+        );
+      },
+    );
+  }
+}
