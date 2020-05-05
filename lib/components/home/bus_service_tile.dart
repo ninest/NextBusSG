c5f8e0c91fc3895cb1517db76152d1e8363238aa
themindstorm <parth.kabra@gmail.com>
2020-05-05 16:24:10 +0800

    added slide to favorite and unfavorite

--- lib/components/home/bus_service_tile.dart
+++ lib/components/home/bus_service_tile.dart
@@ -1,11 +1,15 @@
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nextbussg/components/home/favorites/confirmation_bottom_sheet.dart';
 import 'package:nextbussg/components/search/service_page/service_page.dart';
+import 'package:nextbussg/providers/favorites.dart';
 import 'package:nextbussg/styles/tile_color.dart';
 import 'package:nextbussg/styles/transit_colors.dart';
 import 'package:nextbussg/styles/values.dart';
 import 'package:flutter/material.dart';
 import 'package:nextbussg/models/bus_arrival.dart';
 import 'package:nextbussg/utils/route.dart';
+import 'package:provider/provider.dart';
 
 class BusServiceTile extends StatelessWidget {
   final String code;
@@ -22,60 +26,112 @@ class BusServiceTile extends StatelessWidget {
       "LSD": TransitColors.limited,
     };
 
-    return Container(
-      child: InkWell(
-        child: Padding(
-          padding: EdgeInsets.all(Values.busStopTileHorizontalPadding),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Flexible(
-                flex: 2,
-                child: InkWell(
-                  onTap: () => Routing.openRoute(context, ServicePage(service: service)),
-                  child: Text(
-                    service,
-                    style: Theme.of(context).textTheme.display3,
+    final FavoritesProvider favoritesProvider =
+        Provider.of<FavoritesProvider>(context, listen: true);
+    bool isFavorite = FavoritesProvider.alreadyInFavorites(code, service);
+
+    return Slidable(
+      closeOnScroll: true,
+      key: ObjectKey(service),
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      secondaryActions: <Widget>[
+        _actionTemplate(
+          context,
+
+          // show different icon and color if it's in favorite
+          isFavorite ? FontAwesomeIcons.heartBroken : FontAwesomeIcons.heart,
+          isFavorite ? Theme.of(context).errorColor : Theme.of(context).accentColor,
+
+          // below function adds or remove favorites depending on if it's already
+          // favorited
+          () => favoritesProvider.addToFavorite(context, code, service),
+        )
+      ],
+      child: Container(
+        child: InkWell(
+          child: Padding(
+            padding: EdgeInsets.all(Values.busStopTileHorizontalPadding),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Flexible(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: () => Routing.openRoute(context, ServicePage(service: service)),
+                    child: Text(
+                      service,
+                      style: Theme.of(context).textTheme.display3,
+                    ),
                   ),
                 ),
-              ),
-              Flexible(
-                flex: 5,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    for (var nextBus in busArrival.nextBuses)
-                      Text(
-                        nextBus.timeInMinutes ?? '-',
-                        style: Theme.of(context).textTheme.display4.copyWith(
-                            color: loadColors[nextBus.load],
+                Flexible(
+                  flex: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      for (var nextBus in busArrival.nextBuses)
+                        Text(
+                          nextBus.timeInMinutes ?? '-',
+                          style: Theme.of(context).textTheme.display4.copyWith(
+                              color: loadColors[nextBus.load],
 
-                            // bold text if it has arrived
-                            fontWeight:
-                                nextBus.timeInMinutes == "Arr" ? FontWeight.w900 : FontWeight.w400),
-                      ),
-                  ],
-                ),
-              )
-            ],
+                              // bold text if it has arrived
+                              fontWeight: nextBus.timeInMinutes == "Arr"
+                                  ? FontWeight.w900
+                                  : FontWeight.w400),
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        borderRadius: BorderRadius.circular(Values.borderRadius * 0.8),
-        onTap: () => ConfirmationBottomSheets.confirmAction(context, code, service),
+          borderRadius: BorderRadius.circular(Values.borderRadius * 0.8),
+          onTap: () => ConfirmationBottomSheets.confirmAction(context, code, service),
 
-        // these actons can be something else
-        // onLongPress: () => ConfirmationBottomSheets.confirmAction(context, code, service),
-        // onDoubleTap: () => ConfirmationBottomSheets.confirmAction(context, code, service),
+          // these actons can be something else
+          // onLongPress: () => ConfirmationBottomSheets.confirmAction(context, code, service),
+          // onDoubleTap: () => ConfirmationBottomSheets.confirmAction(context, code, service),
+        ),
+        margin: EdgeInsets.only(
+          left: Values.pageHorizontalPadding,
+          right: Values.pageHorizontalPadding,
+          bottom: Values.pageHorizontalPadding,
+        ),
+        decoration: BoxDecoration(
+          color: TileColors.busServiceTile(context),
+          borderRadius: BorderRadius.circular(Values.borderRadius * 0.8),
+        ),
       ),
-      margin: EdgeInsets.only(
-        left: Values.pageHorizontalPadding,
-        right: Values.pageHorizontalPadding,
-        bottom: Values.pageHorizontalPadding,
+    );
+  }
+
+  Widget _actionTemplate(
+    BuildContext context,
+    IconData icon,
+    Color color,
+    Function action,
+  ) {
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(
+          left: Values.pageHorizontalPadding,
+          right: Values.pageHorizontalPadding,
+          bottom: Values.pageHorizontalPadding,
+        ),
+        decoration: BoxDecoration(
+          // color: TileColors.busServiceTile(context),
+          color: color,
+          borderRadius: BorderRadius.circular(Values.borderRadius * 0.8),
+        ),
+        padding: EdgeInsets.all(Values.marginBelowTitle),
+        child: Icon(
+          icon,
+          color: Colors.white,
+        ),
       ),
-      decoration: BoxDecoration(
-        color: TileColors.busServiceTile(context),
-        borderRadius: BorderRadius.circular(Values.borderRadius * 0.8),
-      ),
+      onTap: action,
     );
   }
 }
